877c31cfc43145a8e33190661c94287a23d3faac
Ankitverma <143443872+ankitver7@users.noreply.github.com>
2026-01-22 14:58:44 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1ehE7ZVRs6mIbqYOaAqndJeaHwV40m2Lb/view"
+            href="https://drive.google.com/file/d/1NLtuqqopcSRRMoGm7VxrnprgpQgKJ2oP/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
